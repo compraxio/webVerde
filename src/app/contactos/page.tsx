ed55e237cbfc30a4fe4725fec4266f45755da386
compraxio <luisangelacu10@gmail.com>
2026-02-10 19:59:25 -0500

    prueva

--- src/app/contactos/page.tsx
+++ src/app/contactos/page.tsx
@@ -1,6 +1,6 @@
 
 import { CuerpoContactos } from '@/components/CuerpoContactos';
-import { Suspense } from 'react';
+import Link from 'next/link';
 
 export default function Contactos() {
   return (
@@ -14,10 +14,11 @@ export default function Contactos() {
             Gestiona la red de contactos vinculada a los Negocios Verdes.
           </p>
         </div>
-        <button className="bg-primary hover:bg-primary/90 text-background-dark font-bold px-5 py-2.5 rounded-lg flex items-center justify-center gap-2 transition-all shadow-lg shadow-primary/20">
-          {/*<span className="material-icons text-lg">person_add</span>*/}
-          <span>Agregar Contacto</span>
-        </button>
+        <Link href="/crear" className="bg-primary hover:bg-primary/90 text-background-dark font-bold px-5 py-2.5 rounded-lg flex items-center justify-center gap-2 transition-all shadow-lg shadow-primary/20" >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="size-6" strokeWidth={1.5} viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0M3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.3 12.3 0 0 1 9.374 21C7.043 21 4.862 20.355 3 19.234Z"/></svg>
+
+          Agregar Contacto
+        </Link>
       </div>
       <div className="bg-white dark:bg-slate-900 rounded-xl border border-slate-200 dark:border-slate-800 shadow-sm overflow-hidden">
         <div className="overflow-x-auto">
--- src/components/CuerpoContactos.tsx
+++ src/components/CuerpoContactos.tsx
@@ -1,11 +1,14 @@
 'use client';
 
-import type { ContactosResponse } from '@/types/contactosType';
-import { useQuery, useQueryClient, useMutation} from '@tanstack/react-query';
-export function CuerpoContactos() {
-  const queryCliente = useQueryClient()
+import { Skeleton } from './ui/skeleton';
 
-  const { data } = useQuery<ContactosResponse>({
+import type { ContactosResponse } from '@/types/contactosType';
+import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query';
+
+export function CuerpoContactos() {
+  const queryCliente = useQueryClient();
+
+  const { data, isPending } = useQuery<ContactosResponse>({
     queryKey: ['contactos'],
     queryFn: async () => {
       const res = await fetch('https://api-base-de-datos.vercel.app/contactos/');
@@ -13,14 +16,36 @@ export function CuerpoContactos() {
     },
   });
 
-  const contacoMutado = useMutation({
+  const deleteMutation = useMutation({
     mutationFn: async (id: number) => {
-      await fetch(`https://api-base-de-datos.vercel.app/contactos/${id}`, {method: "DELETE"});
+      await fetch(`https://api-base-de-datos.vercel.app/contactos/${id}`, { method: 'DELETE' });
     },
     onSuccess: () => {
       queryCliente.invalidateQueries({ queryKey: ['contactos'] });
-    }
-  })
+    },
+  });
+
+  if (isPending) {
+    return (
+      <tr className="hover:bg-slate-50/50 dark:hover:bg-slate-800/30 transition-colors">
+        <td className="px-6 py-4">
+          <Skeleton className="h-7.25 w-61.5" />
+        </td>
+
+        <td className="px-6 py-4 text-sm text-slate-600 dark:text-slate-400">
+          <Skeleton className="h-7.25 w-61.5" />
+        </td>
+
+        <td className="px-6 py-4 text-sm text-slate-600 dark:text-slate-400">
+          <Skeleton className="h-7.25 w-61.5" />
+        </td>
+
+        <td className="px-6 py-4 text-sm text-slate-600 dark:text-slate-400 text-right">
+          <Skeleton className="h-6 w-6" />
+        </td>
+      </tr>
+    );
+  }
   return (
     <>
       {data?.data.map((c) => (
@@ -39,10 +64,23 @@ export function CuerpoContactos() {
           <td className="px-6 py-4 text-sm text-slate-600 dark:text-slate-400 text-right">
             <button
               onClick={() => {
-                contacoMutado.mutate(c.id_contacto);
+                deleteMutation.mutate(c.id_contacto);
               }}
             >
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="size-6" strokeWidth={1.5} viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21q.512.078 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48 48 0 0 0-3.478-.397m-12 .562q.51-.089 1.022-.165m0 0a48 48 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a52 52 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a49 49 0 0 0-7.5 0"/></svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                stroke="currentColor"
+                className="size-6"
+                strokeWidth={1.5}
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21q.512.078 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48 48 0 0 0-3.478-.397m-12 .562q.51-.089 1.022-.165m0 0a48 48 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a52 52 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a49 49 0 0 0-7.5 0"
+                />
+              </svg>
             </button>
           </td>
         </tr>
