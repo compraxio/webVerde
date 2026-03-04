9e8a83d055a89019ec9cb49d745c31c29bb3c504
compraxio <luisangelacu10@gmail.com>
2026-03-03 21:21:18 -0500

    Cosas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -81,6 +81,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" suppressHydrationWarning>
+      <meta name="google-site-verification" content="_py-3iSk1bVaogo4aCahCvxdLHcFIPdRE8URklpBRr8" />
       <body
         className={`${inter.className} bg-background-light  dark:bg-background-dark text-slate-800 dark:text-slate-100 min-h-screen transition-colors duration-200`}
       >
